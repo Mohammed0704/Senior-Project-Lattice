607f0df15ce83f58d879ab73278cff61ce14c9c9
HaloSeventy7 <njmalenfant@gmail.com>
2022-11-17 16:16:23 -0500

    Improved time generation and made student choices randomized

--- data_generation/GenerateLibraryLogFile.py
+++ data_generation/GenerateLibraryLogFile.py
@@ -7,28 +7,39 @@ from datetime import timedelta
 
 currentDirectory = os.getcwd()
 
+year = 2021
+month = random.randint(1, 3)
+day = random.randint(1, 28)
+hour = random.randint(1,23)
+minute = random.randint(1, 59)
+second = random.randint(1, 59)
+lineDateTime = datetime(year, month, day, hour, minute, second)
+
 #Reading csv file with student data into a list of lists
 file = open(currentDirectory + "/MariaDB_Data/MariaDB-basic_student_info.csv", 'r')
 data = list(csv.DictReader(file, delimiter=","))
 file.close()
 
 #Initializing each list into a variable, using column headers as index
-drexelID = [row["Drexel ID"] for row in data]
-firstName = [row["First Name"] for row in data]
-lastName = [row["Last Name"] for row in data]
+drexelIDList = [row["Drexel ID"] for row in data]
+firstNameList = [row["First Name"] for row in data]
+lastNameList = [row["Last Name"] for row in data]
 
+isFirstIteration = True
 #Writing to text file, iterating over multiple lists simultaneously
 #zip_longest stops when all lists are exhausted. When the shorter iterator(s) are exhausted, zip_longest yields a tuple with None value.
-with open('LibraryLogFile.txt', 'w') as f:
-    for (ID, first, last) in itertools.zip_longest(drexelID, firstName, lastName):
-        #Getting the current date and time, incrementing it by random num of seconds and converting it to a string
-        dt = datetime.now()
-        incrementDT = timedelta(seconds=random.randint(1, 10000000)) 
-        randomTime = dt + incrementDT
-        strDateTime = randomTime.strftime("%d-%m-%Y %H:%M:%S")
+with open(currentDirectory + "/Elasticsearch_Data/LibraryLogFile.txt", "w") as f:
+    #for (ID, first, last) in itertools.zip_longest(drexelID, firstName, lastName):
+    for line in range(8500):
+        randomStudentChoice = random.randint(0, len(drexelIDList) - 1)
+        if not isFirstIteration:
+            timeAddition = timedelta(hours=random.randint(0,3), minutes=random.randint(1,59), seconds=random.randint(1,59))
+            lineDateTime = lineDateTime + timeAddition
+        else:
+            isFirstIteration = False
 
-        f.write("[" + strDateTime + "]" + " " + "|" + " " + first + " " + last + " " + "|" + " " 
-        + "Drexel ID:" + " " + ID + " " + "|" + " " + "checked-in" + " " + "|" + " " + "W. W. Hagerty Library")
+        f.write("[" + str(lineDateTime) + "]" + " " + "|" + " " + firstNameList[randomStudentChoice] + " " + lastNameList[randomStudentChoice] + " " + "|" + " " 
+        + "Drexel ID:" + " " + drexelIDList[randomStudentChoice] + " " + "|" + " " + "scanned in" + " " + "|" + " " + "W. W. Hagerty Library")
         f.write('\n')
         f.write("---------------------")
         f.write('\n')
