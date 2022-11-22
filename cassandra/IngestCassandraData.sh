0a68e7a7d5677c058fa08846eed17925c7b61233
HaloSeventy7 <njmalenfant@gmail.com>
2022-11-22 12:52:29 -0500

    Finished cassandra insert shell script

--- cassandra/IngestCassandraData.sh
+++ cassandra/IngestCassandraData.sh
@@ -39,3 +39,59 @@ docker exec -it cassandra cqlsh -e "COPY finances.health_insurance (drexel_id, w
                 FROM '/data_files/Cassandra-health_insurance.csv' 
                 WITH HEADER = TRUE;"
 
+
+#housing_costs
+docker exec -it cassandra cqlsh -e "CREATE TABLE finances.housing_costs(
+                                    residence_name text PRIMARY KEY, 
+                                    room_style text, 
+                                    eligible_residents text,
+                                    minimum_monthly_cost varint,
+                                    maximum_monthly_cost varint
+                                    );"
+
+docker exec -it cassandra cqlsh -e "COPY finances.housing_costs (residence_name, room_style, eligible_residents, minimum_monthly_cost, maximum_monthly_cost) 
+                FROM '/data_files/Cassandra-housing-costs.csv' 
+                WITH HEADER = TRUE;"
+
+
+#overhead_costs
+docker exec -it cassandra cqlsh -e "CREATE TABLE finances.overhead_costs(
+                                    name_of_building text PRIMARY KEY, 
+                                    monthly_electricity varint, 
+                                    monthly_heating varint,
+                                    monthly_water varint,
+                                    monthly_additional_utilities varint
+                                    );"
+
+docker exec -it cassandra cqlsh -e "COPY finances.overhead_costs (name_of_building, monthly_electricity, monthly_heating, monthly_water, monthly_additional_utilities) 
+                FROM '/data_files/Cassandra-overhead-costs.csv' 
+                WITH HEADER = TRUE;"
+
+
+#software_costs
+docker exec -it cassandra cqlsh -e "CREATE TABLE finances.software_costs(
+                                    software text PRIMARY KEY, 
+                                    drexel_annual_license_cost varint
+                                    );"
+
+docker exec -it cassandra cqlsh -e "COPY finances.software_costs (software, drexel_annual_license_cost) 
+                FROM '/data_files/Cassandra-software_costs.csv' 
+                WITH HEADER = TRUE;"
+
+
+#tuition
+docker exec -it cassandra cqlsh -e "CREATE TABLE finances.tuition(
+                                    drexel_id text PRIMARY KEY, 
+                                    year varint, 
+                                    tuition_and_fees float,
+                                    annual_financial_aid_award float,
+                                    subsidized_loan_offered float,
+                                    unsubsidized_loan_offered float,
+                                    accepted_subsidized boolean,
+                                    accepted_unsubsidized boolean,
+                                    outstanding_balances float
+                                    );"
+
+docker exec -it cassandra cqlsh -e "COPY finances.tuition (drexel_id, year, tuition_and_fees, annual_financial_aid_award, subsidized_loan_offered, unsubsidized_loan_offered, accepted_subsidized, accepted_unsubsidized, outstanding_balances) 
+                FROM '/data_files/Cassandra-tuition.csv' 
+                WITH HEADER = TRUE;"
