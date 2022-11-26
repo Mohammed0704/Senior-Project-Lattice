3a561a8cbd0c06baf7a0f30be00c46622751e40c
HaloSeventy7 <njmalenfant@gmail.com>
2022-11-26 17:39:57 -0500

    Finished master ingest script

---
+++ IngestDataIntoDatabases.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+python ./elasticsearch/ElasticFileConversion.py
+
+./cassandra/IngestCassandraData.sh
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo -e "\nData successfully ingested into Cassandra!"
+else
+    echo -e "\nData failed to ingest into Cassandra! Exiting..."
+    exit 1
+fi
+
+./mariadb/IngestMariaDBData.sh
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo -e "\nData successfully ingested into MariaDB!"
+else
+    echo -e "\nData failed to ingest into MariaDB! Exiting..."
+    exit 1
+fi
+
+./postgres/IngestPostgresData.sh
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo -e "\nData successfully ingested into Postgres!"
+else
+    echo -e "\nData failed to ingest into Postgres! Exiting..."
+    exit 1
+fi
+
+./elasticsearch/IngestElasticsearchData.sh
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo -e "\nData successfully ingested into Elasticsearch!"
+else
+    echo -e "\nData failed to ingest into Elasticsearch! Exiting..."
+    exit 1
+fi
