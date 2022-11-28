acf85ae3fb5dbb82eb57fa7e9f3db609bda8960c
HaloSeventy7 <njmalenfant@gmail.com>
2022-11-28 18:17:56 -0500

    Finished mongo ingest script

--- mongodb/ingestMongoData.sh
+++ mongodb/ingestMongoData.sh
@@ -2,21 +2,16 @@
 
 #copy mariadb data into the mariadb container
 docker cp ./mongodb/data_files mongo:/
-
 docker cp ./mongodb/require.js mongo:/
-docker exec -it mongo mongosh -u root -p root --eval "use classes"
-docker exec -it mongo mongosh -u root -p root --eval "db.dropDatabase()"
-docker exec -it mongo mongosh -u root -p root --eval "use classes"
 
-docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
-db.software.insertMany(courseSoftwareJson);
-db.classes_info.insertMany(classInfoJson);
-db.classes_instruction_info.insertMany(classInstrInfoJson);"
+docker exec -it mongo mongosh -u root -p root --eval "use classes" --eval "db.dropDatabase()"
 
-docker exec -it mongo mongosh -u root -p root --eval "use courses"
-docker exec -it mongo mongosh -u root -p root --eval "db.dropDatabase()"
-docker exec -it mongo mongosh -u root -p root --eval "use courses"
+docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.software.insertMany(courseSoftwareJson)"
+docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_info.insertMany(classInfoJson)"
+docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_instruction_info.insertMany(classInstrInfoJson)"
 
-docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
-db.courses_info.insertMany(courseInfoJson);
-db.courses_requirements.insertMany(courseReqsJson);"
+
+docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "db.dropDatabase()"
+
+docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_info.insertMany(courseInfoJson)"
+docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_requirements.insertMany(courseReqsJson)"
