2a75b455ff0a1d2df0816cbe285d5865cb17a715
Matt D'Ulisse <mattd0312@gmail.com>
2022-11-28 16:55:52 -0500

    merged main and modified Ingest

--- mongodb/ingestMongoData.sh
+++ mongodb/ingestMongoData.sh
@@ -4,16 +4,18 @@
 docker cp ./mongodb/data_files mongo:/
 
 docker cp ./mongodb/require.js mongo:/
-docker exec -it mongo mongosh -u root -p root --eval "use classes
-db.dropDatabase()
-use classes"
+docker exec -it mongo mongosh -u root -p root --eval "use classes"
+docker exec -it mongo mongosh -u root -p root --eval "db.dropDatabase()"
+docker exec -it mongo mongosh -u root -p root --eval "use classes"
 
 docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
-db.software.insertMany(courseSoftwareJson);"
+db.software.insertMany(courseSoftwareJson);
+db.classes_info.insertMany(classInfoJson);
+db.classes_instruction_info.insertMany(classInstrInfoJson);"
 
-docker exec -it mongo mongosh -u root -p root --eval "use courses
-db.dropDatabase()
-use courses"
+docker exec -it mongo mongosh -u root -p root --eval "use courses"
+docker exec -it mongo mongosh -u root -p root --eval "db.dropDatabase()"
+docker exec -it mongo mongosh -u root -p root --eval "use courses"
 
 docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
 db.courses_info.insertMany(courseInfoJson);
--- mongodb/require.js
+++ mongodb/require.js
@@ -1,3 +1,5 @@
 const courseInfoJson = require("./data_files/MongoDB-course-info.json");
 const courseReqsJson = require("./data_files/MongoDB-course-restrictions.json");
+const classInfoJson = require("./data_files/MongoDB-class-info.json");
+const classInstrInfoJson = require("./data_files/MongoDB-class-instruction-info.json");
 const courseSoftwareJson = require("./data_files/MongoDB-software.json");
