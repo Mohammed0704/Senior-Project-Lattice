4fef1469a719c7cb9a5062cb9db3367590629128
Matt D'Ulisse <mattd0312@gmail.com>
2022-11-28 16:47:36 -0500

    added drop before inserts

--- mongodb/ingestMongoData.sh
+++ mongodb/ingestMongoData.sh
@@ -4,14 +4,17 @@
 docker cp ./mongodb/data_files mongo:/
 
 docker cp ./mongodb/require.js mongo:/
-
-docker exec -it mongo mongosh -u root -p root --eval "use classes"
+docker exec -it mongo mongosh -u root -p root --eval "use classes
+db.dropDatabase()
+use classes"
 
 docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
 db.software.insertMany(courseSoftwareJson);"
 
+docker exec -it mongo mongosh -u root -p root --eval "use courses
+db.dropDatabase()
+use courses"
 
-docker exec -it mongo mongosh -u root -p root --eval "use courses"
 docker exec -it mongo mongosh -u root -p root --eval "load('require.js');
 db.courses_info.insertMany(courseInfoJson);
 db.courses_requirements.insertMany(courseReqsJson);"
