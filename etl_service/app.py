bb007463ecb4b1bae80b6f2eba80669f81533924
HaloSeventy7 <njmalenfant@gmail.com>
2022-12-10 20:10:25 -0500

    example templates with variable values assigned

--- etl_service/app.py
+++ etl_service/app.py
@@ -1,10 +1,23 @@
 #JUST SOME DEFAULT TESTING THING, CALLED IN etl-flask-dockerfile
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
     return 'Hello World'
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/index")
+def index():
+    return "index"
+
+@app.route("/about")
+def about():
+    sites = ['twitter', 'facebook', 'instagram', 'whatsapp']
+    return render_template("about.html", sites=sites)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
