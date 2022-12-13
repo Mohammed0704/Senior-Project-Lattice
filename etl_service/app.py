f290cd30a5cd4b65132de96528f8c10e21972de8
HaloSeventy7 <njmalenfant@gmail.com>
2022-12-12 20:09:40 -0500

    Created the home page

--- etl_service/app.py
+++ etl_service/app.py
@@ -1,14 +1,30 @@
 #JUST SOME DEFAULT TESTING THING, CALLED IN etl-flask-dockerfile
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def base():
+    return redirect(url_for("home"))
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home_page.html")
+
+@app.route("/connections")
+def connections():
+    return "connections"
+
+@app.route("/tags")
+def tags():
+    return "tags"
+
+@app.route("/metadata")
+def metadata():
+    return "metadata"
+
+@app.route("/loader")
+def loader():
+    return "loader"
 
 @app.route("/index")
 def index():
@@ -24,5 +40,5 @@ def dropdown():
     colors = ['Red', 'Blue', 'Black', 'Orange']
     return render_template('dropdown.html', colors=colors)
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
