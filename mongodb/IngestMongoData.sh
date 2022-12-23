3d71f9b25fd8506317414bd2631acf66800fcef0
HaloSeventy7 <njmalenfant@gmail.com>
2022-12-22 22:22:00 -0500

    Have all connected except for ES, added winpty for windows for ingest scripts, added port to maria in docker-compose

--- mongodb/IngestMongoData.sh
+++ mongodb/IngestMongoData.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
-#copy mariadb data into the mariadb container
-docker cp ./mongodb/data_files mongo:/
-docker cp ./mongodb/require.js mongo:/
+#checks if the machine is Windows and provides alternate commands if so
+test winpty
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
 
-docker exec -it mongo mongosh -u root -p root --eval "use classes" --eval "db.dropDatabase()"
+    #copy mariadb data into the mariadb container
+    docker cp ./mongodb/data_files mongo:/
+    docker cp ./mongodb/require.js mongo:/
 
-docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.software.insertMany(courseSoftwareJson)"
-docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_info.insertMany(classInfoJson)"
-docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_instruction_info.insertMany(classInstrInfoJson)"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use classes" --eval "db.dropDatabase()"
+
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.software.insertMany(courseSoftwareJson)"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_info.insertMany(classInfoJson)"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_instruction_info.insertMany(classInstrInfoJson)"
 
 
-docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "db.dropDatabase()"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "db.dropDatabase()"
 
-docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_info.insertMany(courseInfoJson)"
-docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_requirements.insertMany(courseReqsJson)"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_info.insertMany(courseInfoJson)"
+    winpty docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_requirements.insertMany(courseReqsJson)"
+else
+        #copy mariadb data into the mariadb container
+    docker cp ./mongodb/data_files mongo:/
+    docker cp ./mongodb/require.js mongo:/
+
+    docker exec -it mongo mongosh -u root -p root --eval "use classes" --eval "db.dropDatabase()"
+
+    docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.software.insertMany(courseSoftwareJson)"
+    docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_info.insertMany(classInfoJson)"
+    docker exec -it mongo mongosh -u root -p root --eval "use classes;" --eval "load('require.js')" --eval "db.classes_instruction_info.insertMany(classInstrInfoJson)"
+
+
+    docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "db.dropDatabase()"
+
+    docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_info.insertMany(courseInfoJson)"
+    docker exec -it mongo mongosh -u root -p root --eval "use courses;" --eval "load('require.js')" --eval "db.courses_requirements.insertMany(courseReqsJson)"
+fi
