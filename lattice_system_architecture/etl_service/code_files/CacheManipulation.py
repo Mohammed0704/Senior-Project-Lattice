8b0005b969e2d48963eb54d016d041141add1da0
Halo-nm <njmalenfant@gmail.com>
2023-01-17 14:28:14 -0500

    Added dummy info to test the functions

--- lattice_system_architecture/etl_service/code_files/CacheManipulation.py
+++ lattice_system_architecture/etl_service/code_files/CacheManipulation.py
@@ -21,6 +21,26 @@ def DeleteFromCache(filepath, dict):
         print("Value could not be found in {}.".format(filepath))
 
 
-# if __name__ == "__main__":
-    #AppendToCache("./Connections.txt", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
-    # DeleteFromCache("./Connections.txt", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
+if __name__ == "__main__":
+    connectionTestDict = [
+                            {
+                            "connection_name": "MDB",
+                            "connection_type": "MariaDB",
+                            "connection_URL": "196.22.77.108:3306",
+                            "connection_username": "user",
+                            "connection_password": "pass"
+                            },
+                            {
+                            "connection_name": "MonDB",
+                            "connection_type": "MongoDB",
+                            "connection_URL": "196.22.77.108:27017",
+                            "connection_username": "root",
+                            "connection_password": "password"
+                            }
+                        ]
+    Serialize(connectionTestDict, "./ConnectionsTest.txt")
+    
+    AppendToCache("./ConnectionsTest.txt", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
+    DeleteFromCache("./ConnectionsTest.txt", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
+
+    print(Deserialize("./ConnectionsTest.txt"))
