e9290440a12a483fec987ef5635f32a1cd38ff5d
Halo-nm <njmalenfant@gmail.com>
2023-01-18 16:16:06 -0500

    Added key name and value checks, abstracted Check function

--- lattice_system_architecture/etl_service/code_files/CacheManipulation.py
+++ lattice_system_architecture/etl_service/code_files/CacheManipulation.py
@@ -1,32 +1,59 @@
 import os
 from Serialization import Serialize
-from Serialization import Deserialize 
+from Serialization import Deserialize
+
+# Check if the key value exists and if found, return the entire dictionary where the key value is found
+def CheckIfKeyValueAlreadyExists(keyName, keyValue, cacheData):
+    for dict in cacheData:
+        if dict[keyName] == keyValue:
+            return dict
+    return None
 
 # Append full dict to cache file. Create empty cache file if it doesn't already exist
-def AppendToCache(filepath, dict):
+def AppendToCache(filepath, keyName, dict):
     if not os.path.exists(filepath):
         open(filepath, "x")
     cacheData = Deserialize(filepath)
-    cacheData.append(dict)
-    Serialize(cacheData, filepath)
+
+    # if the key name is not expected to be in the current cache structure
+    if len(cacheData) > 0: 
+        if cacheData[0].get(keyName) == None:
+            print("Failure to append: Key name \"{}\" could not be found in \"{}\"".format(keyName, filepath))
+            return
+        if dict.get(keyName) == None:
+            print("Failure to append: Key name \"{}\" could not be found in dict".format(keyName))
+            return
+
+    keyValue = dict[keyName]
+    if not CheckIfKeyValueAlreadyExists(keyName, keyValue, cacheData):
+        cacheData.append(dict)
+        Serialize(cacheData, filepath)
+    else:
+        print("Failure to append: The key value \"{}\" is already in use".format(keyValue))
 
 # Used to remove dict value from cache file based on itemValue of keyName
-def DeleteFromCache(filepath, keyName, itemValue):
+def DeleteFromCache(filepath, keyName, keyValue):
     if not os.path.exists(filepath):
-        print("Cache file {} does not exist.".format(filepath))
+        print("Failure to remove: Cache file \"{}\" does not exist".format(filepath))
         return
-    try:
-        cacheData = Deserialize(filepath)
-        for dict in cacheData:
-            if dict[keyName] == itemValue:
-                cacheData.remove(dict)
-        Serialize(cacheData, filepath)
-    except:
-        print("Key name could not be found in {}.".format(filepath))
+    cacheData = Deserialize(filepath)
 
+    # if the key name is not in the current cache data
+    if len(cacheData) > 0: 
+        if cacheData[0].get(keyName) == None:
+            print("Failure to remove: Key name \"{}\" could not be found in \"{}\"".format(keyName, filepath))
+            return
+
+    dictToRemove = CheckIfKeyValueAlreadyExists(keyName, keyValue, cacheData)
+    if dictToRemove:
+        cacheData.remove(dictToRemove)
+        Serialize(cacheData, filepath)
+    else:
+        print("Failure to remove: The key value \"{}\" does not exist".format(keyValue))
 
 if __name__ == "__main__":
-    connectionTestDict = [
+    #for testing purposes, uncomment the object and Serialize call below temporarily if the test file does not yet exist
+    '''connectionTestDict = [
                             {
                             "connection_name": "MDB",
                             "connection_type": "MariaDB",
@@ -42,9 +69,9 @@ if __name__ == "__main__":
                             "connection_password": "password"
                             }
                         ]
-    Serialize(connectionTestDict, "./ConnectionsTest.txt")
+    Serialize(connectionTestDict, "./ConnectionsTest.txt")'''
     
-    AppendToCache("./ConnectionsTest.txt", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
+    AppendToCache("./ConnectionsTest.txt", "connection_name", {"connection_name": "Post", "connection_type": "Postgres", "connection_URL": "196.22.77.108:5432", "connection_username": "user", "connection_password": ""})
     DeleteFromCache("./ConnectionsTest.txt", "connection_name", "Post")
 
-    print(Deserialize("./ConnectionsTest.txt"))
+    #print(Deserialize("./ConnectionsTest.txt"))
