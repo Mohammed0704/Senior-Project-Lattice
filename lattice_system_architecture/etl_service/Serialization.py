cda624a6c7ca7bd87db85a52babbe538951c74dc
Halo-nm <njmalenfant@gmail.com>
2023-01-20 17:02:50 -0500

    Updated for tag ticket

--- lattice_system_architecture/etl_service/Serialization.py
+++ lattice_system_architecture/etl_service/Serialization.py
@@ -34,59 +34,59 @@ def Deserialize(filePath):
 
 if __name__=="__main__":
     '''taggedColumnsList = [
-                            {
-                            "tag_name": "Housing",
-                            "all_columns": 
-                            [
-                                "cassandra.finances.housing_costs.residence_name", 
-                                "cassandra.finances.housing_costs.room_style", 
-                                "cassandra.finances.housing_costs.maximum_monthly_cost", 
-                                "cassandra.finances.housing_costs.minimum_monthly_cost",
-                                "postgres.campus_life.housing_options.residence_name",
-                                "postgres.campus_life.housing_options.address",
-                                "postgres.campus_life.housing_options.is_affiliated_housing"
-                            ],
-                            "all_concat_columns":
-                            [
-                                {
-                                    "concat_name": "test", "concat_columns": 
-                                [
-                                    {
-                                        "concat_column": "cassandra.finances.housing_costs.room_style",
-                                        "concat_column_order": 1
-                                    },
-                                    {
-                                        "concat_column": "cassandra.finances.housing_costs.maximum_monthly_cost",
-                                        "concat_column_order": 2
-                                    }
-                                ]
-                                },
-                                {
-                                    "concat_name": "another_test", "concat_columns": 
-                                [
-                                    {
-                                        "concat_column": "postgres.campus_life.housing_options.address",
-                                        "concat_column_order": 1
-                                    },
-                                    {
-                                        "concat_column": "postgres.campus_life.housing_options.residence_name",
-                                        "concat_column_order": 2
-                                    }
-                                ]
-                                }
-                            ],
-                            "join_columns":
-                            [
-                                "cassandra.finances.housing_costs.residence_name",
-                                "postgres.campus_life.housing_options.residence_name"
-                            ],
-                            "all_tables":
-                            [
-                                "cassandra.finances.housing_costs",
-                                "postgres.campus_life.housing_options"
-                            ]
-                            }
-                        ]'''
+    {
+    "tag_name": "Housing",
+    "all_columns": 
+    [
+        "cassandra.finances.housing_costs.residence_name", 
+        "cassandra.finances.housing_costs.room_style", 
+        "cassandra.finances.housing_costs.maximum_monthly_cost", 
+        "cassandra.finances.housing_costs.minimum_monthly_cost",
+        "postgres.campus_life.housing_options.residence_name",
+        "postgres.campus_life.housing_options.address",
+        "postgres.campus_life.housing_options.is_affiliated_housing"
+    ],
+    "all_concat_columns":
+    [
+        {
+            "concat_name": "test", "concat_columns": 
+        [
+            {
+                "concat_column": "cassandra.finances.housing_costs.room_style",
+                "concat_column_order": 1
+            },
+            {
+                "concat_column": "cassandra.finances.housing_costs.maximum_monthly_cost",
+                "concat_column_order": 2
+            }
+        ]
+        },
+        {
+            "concat_name": "another_test", "concat_columns": 
+        [
+            {
+                "concat_column": "postgres.campus_life.housing_options.address",
+                "concat_column_order": 1
+            },
+            {
+                "concat_column": "postgres.campus_life.housing_options.residence_name",
+                "concat_column_order": 2
+            }
+        ]
+        }
+    ],
+    "join_columns":
+    [
+        "cassandra.finances.housing_costs.residence_name",
+        "postgres.campus_life.housing_options.residence_name"
+    ],
+    "all_tables":
+    [
+        "cassandra.finances.housing_costs",
+        "postgres.campus_life.housing_options"
+    ]
+    }
+    ]'''
 
     dataSourceConnectionsExample = [
     {
@@ -118,7 +118,37 @@ if __name__=="__main__":
     "connection_password": "admin"
     }
     ]
+
+    tagsExample = [
+    {
+        "tag_name": "Student"
+    }, 
+    {
+        "tag_name": "Student.join"
+    },
+    {
+        "tag_name": "Employee"
+    },
+    {  
+        "tag_name": "Employee.join"
+    },
+    {
+        "tag_name": "Housing"
+    },
+    {
+        "tag_name": "System"
+    },
+    {
+        "tag_name": "Departments"
+    },
+    {
+        "tag_name": "Colleges"
+    },
+    {
+        "tag_name": "Program"
+    }
+    ]
     
-    Serialize(dataSourceConnectionsExample, "./SerializedConnections.txt")
-    deserializedTestObject = Deserialize("./SerializedConnections.txt")
+    Serialize(tagsExample, "./SerializedTags.txt")
+    deserializedTestObject = Deserialize("./SerializedTags.txt")
     print(deserializedTestObject)
