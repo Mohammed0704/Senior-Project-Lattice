e61920a04c4a4d5e17d6ba28b5a35cf5cc7fdf71
Matt D'Ulisse <mattd0312@gmail.com>
2023-02-25 17:59:10 -0500

    Neo4jSetRelationships.py complete

--- lattice_system_architecture/etl_service/code_files/Neo4jSetRelationships.py
+++ lattice_system_architecture/etl_service/code_files/Neo4jSetRelationships.py
@@ -1,24 +1,26 @@
 from neo4j import GraphDatabase
-import Neo4jConnection
+from Neo4jConnection import *
 
 class Neo4jSetRelationships:
-    session = Neo4jConnection.getActiveNeo4jSession()
-    driver = GraphDatabase.driver("neo4j://neo4j:7687")#, auth=("neo4j", "password"))
-    relationsPath = "../resources/relationships.txt"
+  session = Neo4jConnection.getActiveNeo4jSession()
+  relationsPath = "../resources/relationships.txt"
 
-    def setRelationships(self):
-      commands = self.parseFile()
-      for cmd in commands:
-        self.session.run(cmd)
+  def setRelationships(self):
+    commands = self.parseFile()
+    for cmd in commands:
+      self.session.run(cmd)
 
-    def parseFile(self):
-      s = ""
-      cmds= []
-      with open(self.relationsPath, "r") as f:
-        for line in f.readlines():
-          if line.rstrip("\n") != "//:":
-            s.append(line)
-          else:
-            cmds.append(s)
-            s = ""
-      return cmds
+  def parseFile(self):
+    s = ""
+    cmds= []
+    with open(self.relationsPath, "r") as f:
+      for line in f.readlines():
+        if line.rstrip("\n") != "//:":
+          s += line
+        else:
+          cmds.append(s)
+          s = ""
+    return cmds
+    
+if __name__=="__main__": #for testing
+  Neo4jSetRelationships().setRelationships()
