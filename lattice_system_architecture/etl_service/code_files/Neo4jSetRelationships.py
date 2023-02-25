3461f2ff64964df2300c78970865540ff7f75748
Matt D'Ulisse <mattd0312@gmail.com>
2023-02-25 16:14:02 -0500

    structure complete

--- lattice_system_architecture/etl_service/code_files/Neo4jSetRelationships.py
+++ lattice_system_architecture/etl_service/code_files/Neo4jSetRelationships.py
@@ -3,8 +3,22 @@ import Neo4jConnection
 
 class Neo4jSetRelationships:
     session = Neo4jConnection.getActiveNeo4jSession()
-    driver = GraphDatabase.driver("neo4j://neo4j:7687", auth=("neo4j", "password"))
+    driver = GraphDatabase.driver("neo4j://neo4j:7687")#, auth=("neo4j", "password"))
+    relationsPath = "../resources/relationships.txt"
 
-    @staticmethod
-    def setRelationships():
-      
+    def setRelationships(self):
+      commands = self.parseFile()
+      for cmd in commands:
+        self.session.run(cmd)
+
+    def parseFile(self):
+      s = ""
+      cmds= []
+      with open(self.relationsPath, "r") as f:
+        for line in f.readlines():
+          if line.rstrip("\n") != "//:":
+            s.append(line)
+          else:
+            cmds.append(s)
+            s = ""
+      return cmds
